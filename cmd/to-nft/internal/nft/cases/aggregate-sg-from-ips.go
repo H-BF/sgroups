2aba1b7e11af518eec39acf24f57abade13e548e
EvgenyGRI <eugene_gri@mail.ru>
2023-01-13 00:34:40 +0000

    +IPsBySG agg

---
+++ cmd/to-nft/internal/nft/cases/aggregate-sg-from-ips.go
@@ -0,0 +1,123 @@
+package cases
+
+import (
+	"context"
+	"net"
+	"sort"
+	"strings"
+	"sync"
+
+	"github.com/H-BF/corlib/pkg/parallel"
+	"github.com/H-BF/corlib/pkg/slice"
+	sgAPI "github.com/H-BF/protos/pkg/api/sgroups"
+	conv "github.com/H-BF/sgroups/internal/api/sgroups"
+	model "github.com/H-BF/sgroups/internal/models/sgroups"
+	"github.com/c-robinson/iplib"
+	"github.com/pkg/errors"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type (
+	//SgName is a type alias
+	SgName = string
+
+	//SG is a type alias
+	SG = model.SecurityGroup
+
+	//IPsBySG IPs by SG aggreagate list
+	IPsBySG []*itemIPsBySG
+
+	//SGClient is a type alias
+	SGClient = sgAPI.SecGroupServiceClient
+
+	itemIPsBySG = struct {
+		SG
+		IPs iplib.ByIP
+	}
+)
+
+// Load it loads IPs by SG aggregate
+func (obj *IPsBySG) Load(ctx context.Context, client SGClient, ips []net.IP) error {
+	if len(ips) == 0 {
+		return nil
+	}
+	type item = struct {
+		SG
+		IPs iplib.ByIP
+	}
+	agg := make(map[SgName]*item)
+	var mx sync.Mutex
+	job := func(i int) error {
+		req := &sgAPI.GetSecGroupForAddressReq{
+			Address: ips[i].String(),
+		}
+		resp, err := client.GetSecGroupForAddress(ctx, req)
+		if status.Code(err) == codes.NotFound {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		var sg SG
+		if sg, err = conv.Proto2ModelSG(resp); err != nil {
+			return err
+		}
+		mx.Lock()
+		defer mx.Unlock()
+		it := agg[sg.Name]
+		if it == nil {
+			it = new(item)
+			agg[sg.Name] = it
+		}
+		it.IPs = append(it.IPs, ips[i])
+		return nil
+	}
+	if err := parallel.ExecAbstract(len(ips), 7, job); err != nil {
+		errors.WithMessage(err, "load/IPsBySG")
+		return err
+	}
+	for _, it := range agg {
+		*obj = append(*obj, it)
+	}
+	return nil
+}
+
+// Dedup it deduplicates list an its IPs
+func (obj *IPsBySG) Dedup() {
+	for _, it := range *obj {
+		sort.Sort(it.IPs)
+		_ = slice.DedupSlice(&it.IPs, func(i, j int) bool {
+			return iplib.CompareIPs(it.IPs[i], it.IPs[j]) == 0
+		})
+	}
+	sort.Slice(*obj, func(i, j int) bool {
+		return strings.Compare((*obj)[i].SG.Name, (*obj)[j].SG.Name) < 0
+	})
+	_ = slice.DedupSlice(obj, func(i, j int) bool {
+		return strings.EqualFold((*obj)[i].Name, (*obj)[j].Name)
+	})
+}
+
+// SplitIp4Ip6 ...
+func (obj IPsBySG) SplitIp4Ip6() (v4 IPsBySG, v6 IPsBySG) {
+	for _, src := range obj {
+		it4 := itemIPsBySG{SG: src.SG}
+		it6 := it4
+		for _, ip := range src.IPs {
+			switch len(ip) {
+			case net.IPv4len:
+				it4.IPs = append(it4.IPs, ip)
+			case net.IPv6len:
+				it6.IPs = append(it6.IPs, ip)
+			}
+		}
+		if len(it4.IPs) > 0 {
+			v4 = append(v4, &it4)
+		}
+		if len(it6.IPs) > 0 {
+			v6 = append(v6, &it6)
+		}
+	}
+	return v4, v6
+}
---
+++ internal/api/sgroups/proto2model.go
@@ -0,0 +1,50 @@
+package sgroups
+
+import (
+	sg "github.com/H-BF/protos/pkg/api/sgroups"
+	model "github.com/H-BF/sgroups/internal/models/sgroups"
+	"github.com/pkg/errors"
+)
+
+// ProtoNetwork2Model converts Network (proto --> model)
+func Proto2ModelNetwork(protoNw *sg.Network) (model.Network, error) {
+	const api = "proto2model-Network-conv"
+	var ret network
+	err := ret.from(protoNw)
+	return ret.Network, errors.WithMessage(err, api)
+}
+
+// Proto2ModelSG conv SG (proto --> model)
+func Proto2ModelSG(g *sg.SecGroup) (model.SecurityGroup, error) {
+	const api = "proto2model-SG-conv"
+
+	var ret securityGroup
+	ret.from(g)
+	ret.Networks = ret.Networks[:0]
+	for _, nw := range g.GetNetworks() {
+		n, e := Proto2ModelNetwork(nw)
+		if e != nil {
+			return ret.SecurityGroup, errors.WithMessage(e, api)
+		}
+		ret.Networks = append(ret.Networks, n)
+	}
+	return ret.SecurityGroup, nil
+}
+
+// Proto2ModelSGRule conv SGRule (proto --> model)
+func Proto2ModelSGRule(src *sg.Rule) (model.SGRule, error) {
+	const api = "proto2model-SGRule-conv"
+
+	var ret model.SGRule
+	err := (sgRule{&ret}).from(src)
+	if err != nil {
+		return ret, errors.WithMessage(err, api)
+	}
+	if ret.SgFrom, err = Proto2ModelSG(src.GetSgFrom()); err != nil {
+		return ret, errors.WithMessage(err, api)
+	}
+	if ret.SgFrom, err = Proto2ModelSG(src.GetSgTo()); err != nil {
+		return ret, errors.WithMessage(err, api)
+	}
+	return ret, nil
+}
