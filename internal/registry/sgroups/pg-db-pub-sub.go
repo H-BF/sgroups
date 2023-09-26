aefa1895f3391c69c7e42a2dde20a58e4054c708
EvgenyGRI <eugene_gri@mail.ru>
2023-09-26 13:48:48 +0300

    PR advance

--- internal/registry/sgroups/pg-db-pub-sub.go
+++ internal/registry/sgroups/pg-db-pub-sub.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"time"
 
-	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/jackc/puddle/v2"
 )
@@ -16,6 +15,8 @@ const (
 )
 
 func (dbimp *pgDbRegistry) listenMessages(ctx context.Context) {
+	const timeoutBeforeRetry = 10 * time.Second
+
 	pool, ok := dbimp.pool.Load()
 	for ; ok; pool, ok = dbimp.pool.Load() {
 		err := pool.AcquireFunc(ctx, func(c *pgxpool.Conn) error {
@@ -23,7 +24,6 @@ func (dbimp *pgDbRegistry) listenMessages(ctx context.Context) {
 			if _, err := conn.Exec(ctx, "listen "+NotifyCommit); err != nil {
 				return err
 			}
-
 			for {
 				nt, e := conn.WaitForNotification(ctx)
 				if e != nil {
@@ -34,19 +34,13 @@ func (dbimp *pgDbRegistry) listenMessages(ctx context.Context) {
 				}
 			}
 		})
-		if err != nil {
-			if errors.Is(err, puddle.ErrClosedPool) {
-				// since there is no place where pool restoring when it closed so just return from here
-				return
-			} else if pgconn.SafeToRetry(err) {
-				<-time.After(time.Minute)
-				continue
-			} else if pgconn.Timeout(err) {
-				continue
-			} else {
-				// any other error kind
-				return
-			}
+		if errors.Is(err, puddle.ErrClosedPool) {
+			return
+		}
+		select {
+		case <-ctx.Done():
+			return
+		case <-time.After(timeoutBeforeRetry):
 		}
 	}
 }
