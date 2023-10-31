29d1c2c48bd74ad48c0672c97f26a8287f2695c1
Ivan Andreyko <andreyko.ivan@wb.ru>
2023-11-01 00:39:47 +0400

    fix[tf]: start embed server with corlib

--- cmd/sgroups-tf-v2/internal/provider/base-resource-testsuit.go
+++ cmd/sgroups-tf-v2/internal/provider/base-resource-testsuit.go
@@ -4,20 +4,21 @@ import (
 	"bytes"
 	"context"
 	"fmt"
-	"net"
 	"os"
 	"path"
 	"strings"
 	"time"
 
 	sgAPI "github.com/H-BF/sgroups/internal/api/sgroups"
-	client "github.com/H-BF/sgroups/internal/grpc-client"
 	registry "github.com/H-BF/sgroups/internal/registry/sgroups"
-	"google.golang.org/grpc"
 
+	pkgNet "github.com/H-BF/corlib/pkg/net"
+	corlib "github.com/H-BF/corlib/server"
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 	"github.com/stretchr/testify/suite"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 type (
@@ -27,15 +28,14 @@ type (
 		ctx               context.Context
 		ctxCancel         func()
 		server            *embedServer
-		sgClient          sgAPI.ClosableClient
+		closeClient       func() error
+		sgClient          sgAPI.Client
 		providerConfig    string
 		providerFactories map[string]func() (tfprotov6.ProviderServer, error)
 	}
 
 	embedServer struct {
-		Addr       string
-		socketPath string
-		grpcServer *grpc.Server
+		Addr string
 	}
 )
 
@@ -46,21 +46,23 @@ func (sui *baseResourceTests) SetupSuite() {
 	}
 
 	sui.server = NewServer()
-	sui.Require().NoError(sui.server.RunDetached(sui.ctx), "run embed server failed")
+	err := sui.server.RunDetached(sui.ctx)
+	sui.Require().NoErrorf(err, "run embed server failed: %s", err)
 
 	sui.providerConfig = `
 		provider "sgroups" {
 			address = ` + sui.server.Addr + `
 		}
 		`
-	dialDuration := lookupEnvWithDefault("SGROUPS_DIAL_DURATION", "10s")
-	connDuration, err := time.ParseDuration(dialDuration)
-	sui.Require().Nil(err)
 
-	builder := client.FromAddress(sui.server.Addr).
-		WithDialDuration(connDuration)
+	con, err := grpc.DialContext(sui.ctx, sui.server.Addr,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock())
 
-	sui.sgClient, err = sgAPI.NewClosableClient(sui.ctx, builder)
+	sui.closeClient = func() error {
+		return con.Close()
+	}
+	sui.sgClient = sgAPI.NewClient(con)
 	sui.Require().NoError(err)
 
 	sui.providerFactories = map[string]func() (tfprotov6.ProviderServer, error){
@@ -69,10 +71,7 @@ func (sui *baseResourceTests) SetupSuite() {
 }
 
 func (sui *baseResourceTests) TearDownSuite() {
-	err := sui.sgClient.CloseConn()
-	sui.Require().NoError(err)
-
-	sui.server.Stop()
+	sui.NoError(sui.closeClient())
 
 	if sui.ctxCancel != nil {
 		sui.ctxCancel()
@@ -92,26 +91,13 @@ func slice2string[T fmt.Stringer](args ...T) string {
 
 func NewServer() *embedServer {
 	server := new(embedServer)
-	server.socketPath = path.Join("/tmp", fmt.Sprintf("test-%v-%v.socket", os.Getpid(), time.Now().Nanosecond()))
-	server.Addr = fmt.Sprintf("unix://%s", server.socketPath)
+	socketPath := path.Join("/tmp", fmt.Sprintf("test-%v-%v.socket", os.Getpid(), time.Now().Nanosecond()))
+	server.Addr = fmt.Sprintf("unix://%s", socketPath)
 	return server
 }
 
-/*//TODO:
-Итак - Иван опять наговнокодил-наизобретал )))))
-- минус в карму
-Так
-    еще раз
-	- ноем сокрушаемся матюгаемся сыплем проклятья заламываем
-	  руки в мольбах к небу
-	- изучаем корлиб с кактусом в прикуску
-	- изучаем как сделано в cmd/sgrpups/main.go
-	- делаем похожим способом
-	- пишем восхитительный код
-*/
-
 func (server *embedServer) RunDetached(ctx context.Context) error {
-	lis, err := net.Listen("unix", server.socketPath)
+	endpoint, err := pkgNet.ParseEndpoint(server.Addr)
 	if err != nil {
 		return err
 	}
@@ -128,22 +114,21 @@ func (server *embedServer) RunDetached(ctx context.Context) error {
 	}
 	service := sgAPI.NewSGroupsService(ctx, registry.NewRegistryFromMemDB(m))
 
-	var opts []grpc.ServerOption
-	server.grpcServer = grpc.NewServer(opts...)
-	if err := service.RegisterGRPC(ctx, server.grpcServer); err != nil {
+	opts := []corlib.APIServerOption{
+		corlib.WithServices(service),
+	}
+
+	apiServer, err := corlib.NewAPIServer(opts...)
+	if err != nil {
 		return err
 	}
 
 	go func() {
-		if err := server.grpcServer.Serve(lis); err != nil {
+		if err := apiServer.Run(ctx, endpoint); err != nil {
 			fmt.Println("server stopped due" + err.Error())
 		}
 	}()
 	os.Setenv("SGROUPS_ADDRESS", server.Addr)
-	fmt.Printf("server started on %s\n", server.socketPath)
+	fmt.Printf("server started on %s\n", server.Addr)
 	return nil
-}
-
-func (server *embedServer) Stop() {
-	server.grpcServer.Stop()
 }
