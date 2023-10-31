63c7a76d206b506d9109b9d7c5b95c87290e8720
Ivan Andreyko <andreyko.ivan@wb.ru>
2023-10-31 14:45:49 +0400

    feat[tf]: move server stuf to its own struct

--- cmd/sgroups-tf-v2/internal/provider/base-resource-testsuit.go
+++ cmd/sgroups-tf-v2/internal/provider/base-resource-testsuit.go
@@ -20,16 +20,24 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
-type baseResourceTests struct {
-	suite.Suite
+type (
+	baseResourceTests struct {
+		suite.Suite
 
-	ctx               context.Context
-	ctxCancel         func()
-	grpcServer        *grpc.Server
-	sgClient          sgAPI.ClosableClient
-	providerConfig    string
-	providerFactories map[string]func() (tfprotov6.ProviderServer, error)
-}
+		ctx               context.Context
+		ctxCancel         func()
+		server            *embedServer
+		sgClient          sgAPI.ClosableClient
+		providerConfig    string
+		providerFactories map[string]func() (tfprotov6.ProviderServer, error)
+	}
+
+	embedServer struct {
+		Addr       string
+		socketPath string
+		grpcServer *grpc.Server
+	}
+)
 
 func (sui *baseResourceTests) SetupSuite() {
 	sui.ctx = context.Background()
@@ -37,25 +45,19 @@ func (sui *baseResourceTests) SetupSuite() {
 		sui.ctx, sui.ctxCancel = context.WithDeadline(sui.ctx, dl)
 	}
 
-	addr := path.Join("/tmp", fmt.Sprintf("test-%v-%v.socket", os.Getpid(), time.Now().Nanosecond()))
-	var opts []grpc.ServerOption
-	sui.grpcServer = grpc.NewServer(opts...)
-	errc := make(chan struct{}, 1)
-	go runServerDetached(sui.ctx, sui.grpcServer, addr, errc)
-	fmt.Printf("server started on %s\n", addr)
+	sui.server = NewServer()
+	sui.Require().NoError(sui.server.RunDetached(sui.ctx), "run embed server failed")
 
-	address := fmt.Sprintf("unix://%s", addr)
-	os.Setenv("SGROUPS_ADDRESS", address)
 	sui.providerConfig = `
 		provider "sgroups" {
-			address = ` + address + `
+			address = ` + sui.server.Addr + `
 		}
 		`
 	dialDuration := lookupEnvWithDefault("SGROUPS_DIAL_DURATION", "10s")
 	connDuration, err := time.ParseDuration(dialDuration)
 	sui.Require().Nil(err)
 
-	builder := client.FromAddress(address).
+	builder := client.FromAddress(sui.server.Addr).
 		WithDialDuration(connDuration)
 
 	sui.sgClient, err = sgAPI.NewClosableClient(sui.ctx, builder)
@@ -70,8 +72,7 @@ func (sui *baseResourceTests) TearDownSuite() {
 	err := sui.sgClient.CloseConn()
 	sui.Require().NoError(err)
 
-	sui.grpcServer.Stop()
-	fmt.Println("server stopped")
+	sui.server.Stop()
 
 	if sui.ctxCancel != nil {
 		sui.ctxCancel()
@@ -89,12 +90,17 @@ func slice2string[T fmt.Stringer](args ...T) string {
 	return strings.ReplaceAll(data.String(), `"`, "'")
 }
 
-func runServerDetached(ctx context.Context, grpcServer *grpc.Server, addr string, errc chan struct{}) {
-	lis, err := net.Listen("unix", addr)
+func NewServer() *embedServer {
+	server := new(embedServer)
+	server.socketPath = path.Join("/tmp", fmt.Sprintf("test-%v-%v.socket", os.Getpid(), time.Now().Nanosecond()))
+	server.Addr = fmt.Sprintf("unix://%s", server.socketPath)
+	return server
+}
+
+func (server *embedServer) RunDetached(ctx context.Context) error {
+	lis, err := net.Listen("unix", server.socketPath)
 	if err != nil {
-		fmt.Printf("cant listen on %s: %s\n", addr, err.Error())
-		errc <- struct{}{}
-		return
+		return err
 	}
 
 	m, err := registry.NewMemDB(registry.TblSecGroups,
@@ -105,15 +111,24 @@ func runServerDetached(ctx context.Context, grpcServer *grpc.Server, addr string
 		registry.IntegrityChecker4FqdnRules(),
 		registry.IntegrityChecker4Networks())
 	if err != nil {
-		fmt.Printf("cant create mem db : %s\n", err.Error())
-		errc <- struct{}{}
-		return
+		return err
 	}
 	service := sgAPI.NewSGroupsService(ctx, registry.NewRegistryFromMemDB(m))
 
-	service.RegisterGRPC(ctx, grpcServer)
-	if err := grpcServer.Serve(lis); err != nil {
-		fmt.Println("server stopped due" + err.Error())
-		errc <- struct{}{}
-	}
+	var opts []grpc.ServerOption
+	server.grpcServer = grpc.NewServer(opts...)
+	service.RegisterGRPC(ctx, server.grpcServer)
+
+	go func() {
+		if err := server.grpcServer.Serve(lis); err != nil {
+			fmt.Println("server stopped due" + err.Error())
+		}
+	}()
+	os.Setenv("SGROUPS_ADDRESS", server.Addr)
+	fmt.Printf("server started on %s\n", server.socketPath)
+	return nil
+}
+
+func (server *embedServer) Stop() {
+	server.grpcServer.Stop()
 }
