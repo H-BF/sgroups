b99c66f95a0c4bd6a7e863cf979fed7137a73126
EvgenyGRI <eugene_gri@mail.ru>
2023-12-15 01:23:28 +0000

    + some comments

--- cmd/to-nft/internal/nft/cases/local-data.go
+++ cmd/to-nft/internal/nft/cases/local-data.go
@@ -2,6 +2,7 @@ package cases
 
 import (
 	"context"
+	"net"
 
 	"github.com/H-BF/sgroups/cmd/to-nft/internal"
 	"github.com/H-BF/sgroups/cmd/to-nft/internal/host"
@@ -10,7 +11,7 @@ import (
 	"github.com/pkg/errors"
 )
 
-// LocalData are used by agent to build local firewall rules
+// LocalData are used by agent to build Host Based Firewall rules
 type LocalData struct {
 	LocalSGs      SGs
 	SG2SGRules    SG2SGRules
@@ -44,8 +45,61 @@ func (loader LocalDataLoader) Load(ctx context.Context, client SGClient, ncnf ho
 		err = errors.WithMessage(err, "LocalData/Load")
 	}()
 
+	var locIPs []net.IP
+	{
+		v4, v6 := ncnf.LocalIPs()
+		locIPs = append(append(locIPs, v4...), v6...)
+	}
+	_ = locIPs
+
 	return res, err
 }
+
+/*//
+localIPsV4, loaclIPsV6 := netConf.LocalIPs()
+allLoaclIPs := append(localIPsV4, loaclIPsV6...)
+log.Infof("start loading data according host net config")
+
+log.Debugw("loading SG...", "host-local-IP(s)", slice2stringer(allLoaclIPs...))
+if applied.LocalSGs, err = impl.loadLocalSGs(ctx, allLoaclIPs); err != nil {
+	return applied, err
+}
+
+stringerOfLocalSGs := slice2stringer(applied.LocalSGs.Names()...)
+log.Debugw("loading SG-SG rules...", "local-SG(s)", stringerOfLocalSGs)
+if localRules, err = impl.loadSgSgRules(ctx, applied.LocalSGs); err != nil {
+	return applied, err
+}
+applied.SG2SGRules = localRules
+
+log.Debugw("loading SG-ICMP rules...", "local-SG(s)", stringerOfLocalSGs)
+if err = applied.SgIcmpRules.Load(ctx, impl.sgClient, applied.LocalSGs); err != nil {
+	return applied, err
+}
+
+log.Debugw("loading SG-SG-ICMP rules...", "local-SG(s)", stringerOfLocalSGs)
+if err = applied.SgSgIcmpRules.Load(ctx, impl.sgClient, applied.LocalSGs); err != nil {
+	return applied, err
+}
+
+log.Debugw("loading SG-FQDN rules...", "local-SG(s)", stringerOfLocalSGs)
+if fqdnRules, err = impl.loadFQDNRules(ctx, applied.LocalSGs); err != nil {
+	return applied, err
+}
+applied.SG2FQDNRules = fqdnRules
+
+log.Debugw("loading CIDR-SG-INGRESS/EGRESS rules...", "local-SG(s)", stringerOfLocalSGs)
+if err = applied.CidrSgRules.Load(ctx, impl.sgClient, applied.LocalSGs); err != nil {
+	return applied, err
+}
+
+sgNames := applied.GetAllUsedSgNames()
+log.Debugw("loading networks...", "SG(s)", slice2stringer(sgNames...))
+if err = networks.LoadFromSGNames(ctx, impl.sgClient, sgNames); err != nil {
+	return applied, err
+}
+log.Info("data loaded; will apply it now")
+*/
 
 /*//
 func stringers(args ...fmt.Stringer) []fmt.Stringer {
