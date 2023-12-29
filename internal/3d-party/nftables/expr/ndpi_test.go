7a183794ea8a004d95d4bfa1be4a074a0b4756a3
EvgenyGRI <eugene_gri@mail.ru>
2023-12-29 20:45:46 +0000

    correct tests

--- internal/3d-party/nftables/expr/ndpi_test.go
+++ internal/3d-party/nftables/expr/ndpi_test.go
@@ -33,9 +33,6 @@ func (sui *ndpiTestSuite) Test_NdpiUnSupportedProtocols() {
 	}
 	mask, err := ndpi.protocolsToBitmask()
 	sui.Require().Error(err)
-	if err == nil {
-		return
-	}
 	sui.Require().Equal(expMask, mask)
 	sui.bitmask2Proto(&mask, nil)
 }
@@ -52,9 +49,6 @@ func (sui *ndpiTestSuite) Test_NdpiOneSupportedProtocol() {
 	}
 	mask, err := ndpi.protocolsToBitmask()
 	sui.Require().NoError(err)
-	if err != nil {
-		return
-	}
 	sui.Require().Equal(expMask, mask)
 	sui.bitmask2Proto(&mask, protocols)
 }
@@ -67,9 +61,6 @@ func (sui *ndpiTestSuite) Test_NdpiSubstractionProtocols() {
 	}
 	mask, err := ndpi.protocolsToBitmask()
 	sui.Require().NoError(err)
-	if err != nil {
-		return
-	}
 	sui.Require().Equal(expMask, mask)
 	sui.bitmask2Proto(&mask, nil)
 }
@@ -86,9 +77,6 @@ func (sui *ndpiTestSuite) Test_NdpiDublicatedProtocols() {
 	}
 	mask, err := ndpi.protocolsToBitmask()
 	sui.Require().NoError(err)
-	if err != nil {
-		return
-	}
 	sui.Require().Equal(expMask, mask)
 	sui.bitmask2Proto(&mask, protocols[:1])
 }
@@ -110,9 +98,6 @@ func (sui *ndpiTestSuite) Test_NdpiSeveralSuportedProtocols() {
 	}
 	mask, err := ndpi.protocolsToBitmask()
 	sui.Require().NoError(err)
-	if err != nil {
-		return
-	}
 	sui.Require().Equal(expMask, mask)
 	//reordered according to bitmask
 	sui.bitmask2Proto(&mask, []string{
@@ -137,75 +122,35 @@ func (sui *ndpiTestSuite) Test_NdpiMixedProtocols() {
 
 	mask, err := ndpi.protocolsToBitmask()
 	sui.Require().Error(err)
-	if err == nil {
-		return
-	}
-
 	sui.Require().Equal(expMask, mask)
 	sui.bitmask2Proto(&mask, protocols[:1])
 }
 
-func (sui *ndpiTestSuite) Test_NdpiMarshalHostOneProto() {
-	expHost := "youtube"
-	expProto := "http"
-	ndpiExp, err := NewNdpi(NdpiWithHost(expHost), NdpiWithProtocols(expProto))
-	sui.Require().NoError(err)
-	m, err := ndpiExp.marshal(0)
-	sui.Require().NoError(err)
-	ndpi, err := NewNdpi()
-	sui.Require().NoError(err)
-	u, err := netlink.UnmarshalAttributes(m)
-	sui.Require().NoError(err)
-	err = ndpi.unmarshal(0, u[1].Data)
-	sui.Require().NoError(err)
-	sui.Require().Equal(ndpiExp, ndpi)
-
-}
-
-func (sui *ndpiTestSuite) Test_NdpiMarshalHostProtos() {
-	expHost := "youtube"
-	expProto := []string{"dns", "http"}
-	ndpiExp, err := NewNdpi(NdpiWithHost(expHost), NdpiWithProtocols(expProto[0], expProto[1]))
-	sui.Require().NoError(err)
-	m, err := ndpiExp.marshal(0)
-	sui.Require().NoError(err)
-	ndpi, err := NewNdpi()
-	sui.Require().NoError(err)
-	u, err := netlink.UnmarshalAttributes(m)
-	sui.Require().NoError(err)
-	err = ndpi.unmarshal(0, u[1].Data)
-	sui.Require().NoError(err)
-	sui.Require().Equal(ndpiExp, ndpi)
-}
-
-func (sui *ndpiTestSuite) Test_NdpiMarshalRegex() {
-	ndpiExp, err := NewNdpi(NdpiWithHost("/youtube/"), NdpiWithProtocols("http"))
-	sui.Require().NoError(err)
-	m, err := ndpiExp.marshal(0)
-	sui.Require().NoError(err)
-	ndpi, err := NewNdpi()
-	sui.Require().NoError(err)
-	u, err := netlink.UnmarshalAttributes(m)
-	sui.Require().NoError(err)
-	err = ndpi.unmarshal(0, u[1].Data)
-	sui.Require().NoError(err)
-	sui.Require().Equal(ndpiExp, ndpi)
-
-}
-
-func (sui *ndpiTestSuite) Test_NdpiMarshalHost() {
-
-	ndpiExp, err := NewNdpi(NdpiWithHost("youtube"))
-	sui.Require().NoError(err)
-	m, err := ndpiExp.marshal(0)
-	sui.Require().NoError(err)
-	ndpi, err := NewNdpi()
-	sui.Require().NoError(err)
-	u, err := netlink.UnmarshalAttributes(m)
-	sui.Require().NoError(err)
-	err = ndpi.unmarshal(0, u[1].Data)
-	sui.Require().NoError(err)
-	sui.Require().Equal(ndpiExp, ndpi)
+func (sui *ndpiTestSuite) Test_NdpiMarshalUnmarshal() {
+	ops := func(o ...ndpiOpt) []ndpiOpt {
+		return o
+	}
+	cases := []struct {
+		opts []ndpiOpt
+	}{
+		{ops(NdpiWithHost("youtube"), NdpiWithProtocols("http"))},
+		{ops(NdpiWithHost("/youtube/"), NdpiWithProtocols("http"))},
+		{ops(NdpiWithHost("youtube"))},
+		{ops(NdpiWithHost("youtube"), NdpiWithProtocols("dns", "http"))},
+	}
+	for _, c := range cases {
+		ndpiExp, err := NewNdpi(c.opts...)
+		sui.Require().NoError(err)
+		m, err := Marshal(0, ndpiExp)
+		sui.Require().NoError(err)
+		u, err := netlink.UnmarshalAttributes(m)
+		sui.Require().NoError(err)
+		sui.Require().Equal(2, len(u))
+		ndpi := new(Ndpi)
+		err = Unmarshal(0, u[1].Data, ndpi)
+		sui.Require().NoError(err)
+		sui.Require().Equal(ndpiExp, ndpi)
+	}
 }
 
 func Test_NDPI(t *testing.T) {
