9728c5b579455bf9741f3b089b5ee04c5d8534b3
Dmitrii Putilin <dlputilin@dobry-kot.ru>
2024-09-14 13:52:59 +0300

    fix docs path

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
 
   // Set the production url of your site here
   url: 'http://localhost',
-  baseUrl: '/sgroups/',
+  baseUrl: '/',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
